e1d5d4904a303b66b431e459bcfb5c97654b92db
Klim Trotsenko <klim.trotsenko7@gmail.com>
2023-07-31 14:35:56 +0300

    update test_hw_lesson_2selen.py

--- lesson_2/test_hw_lesson_2selen.py
+++ lesson_2/test_hw_lesson_2selen.py
@@ -1,21 +1,35 @@
 
-#ДАННЫЙ ЧЕРНОВИК С ПОМОЩЬЮ СЕЛЕНА, НЕТ ВОЗМОЖНОСТИ ПРОВЕРИТЬ, ИЗЗА НЕРАБОТОСПОСОБНОСТИ СЕЛЕНА С НОВОЙ ВЕРСИЕЙ БРАУЗЕРА ХРОМ
 from selene.support.shared import browser
-from selene import be, have
+from selene import be, have, browser
 import pytest
+from selenium import webdriver
+
+
+
+browser.config.driver_options = webdriver.ChromeOptions()
+browser.config.driver_options.binary_location = (
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+
+url = 'https://google.com'
 
 @pytest.fixture()
-def browser_1():
+def window_change():
+    # устанавливаем таймаут у браузера - 10 сек для нестабильного интернета
     browser.config.timeout = 10
+    # удерживаем браузер открытым после выполнения теста
     #browser.config.hold_driver_at_exit = True
-    browser.open('https://google.com')
-
-@pytest.fixture()
-def test_change_window(browser_1):
-    print("Called before each test")
-    # browser.config.window_width(500)
-    # browser.config.window_height(500)
-def test_first(browser_1, change_window):
+    # меняем размеры браузера
+    browser.config.window_height = 1000
+    browser.config.window_width = 1000
+    # открываем браузер по урл
+    browser.open(url)
+    yield browser
+    # закрываем браузер после каждого теста
+    browser.quit()
+def test_check_result(window_change):
     assert browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     assert browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python')), "Результаты содержат текст..."
 
+def test_check_no_result(window_change):
+    assert browser.element('[name="q"]').should(be.blank).type('aslkdhaslgha;lsdhgal;dsg').press_enter()
+    assert browser.element('[id="res"]').should(have.text('No results containing all your search terms were found.')), "Проверка что нет результатов"
